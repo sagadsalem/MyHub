0afbfd6dec00c5beff85364730c797c184e2d64f
Sajad Salem <3su.dev@gmail.com>
2023-07-25 12:41:58 +0300

    Next Config

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,24 @@ const nextConfig = {
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
+  webpack: (config, { isServer }) => {
+    // Add the necessary loader for MDX files
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.mdx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['next/babel'],
+            },
+          },
+          '@mdx-js/loader',
+        ],
+      })
+    }
+    return config
+  },
   options: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
